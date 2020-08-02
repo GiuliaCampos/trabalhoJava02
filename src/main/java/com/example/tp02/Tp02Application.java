7d3b5ad69a0d0d00ccfaa3daa870b5b57414ee67
Weslley de Campos <weslley082@gmail.com>
2020-08-02 20:32:29 -0300

    Enviando alterações nos dados

--- src/main/java/com/example/tp02/Tp02Application.java
+++ src/main/java/com/example/tp02/Tp02Application.java
@@ -1,14 +1,13 @@
 package com.example.tp02;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
-@ComponentScan({"service","controller"})
-
 public class Tp02Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Tp02Application.class, args);
 	}
--- src/main/java/com/example/tp02/controller/FilmeController.java
+++ src/main/java/com/example/tp02/controller/FilmeController.java
@@ -1,27 +1,33 @@
 package com.example.tp02.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.tp02.service.filmeService;
 import com.example.tp02.model.Filme;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class FilmeController {
 
     @Autowired
     filmeService filmeService;
 
     @RequestMapping(value = "/filmes", method = RequestMethod.GET)
-    public ModelAndView getFilmes(){
-        ModelAndView mv = new ModelAndView("filmes");
+    public List<Filme> getFilmes() {
+
         List<Filme> filmes = filmeService.findAll();
-        mv.addObject("filmes", filmes);
-        return mv;
+
+        return filmes;
+    }
+
+    @RequestMapping(value = "/filme/{id_cliente}", method = RequestMethod.GET)
+    public Filme getFilme(@PathVariable Long id_cliente) {
+        Filme filme = filmeService.findById(id_cliente);
+        return filme;
     }
 
 }
