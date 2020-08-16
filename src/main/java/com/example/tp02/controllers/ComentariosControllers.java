c7cfb2ca660e1df77b6578e6a800acbcc5435047
Weslley de Campos <weslley082@gmail.com>
2020-08-16 20:52:36 -0300

    documentacao

--- src/main/java/com/example/tp02/controllers/ComentariosControllers.java
+++ src/main/java/com/example/tp02/controllers/ComentariosControllers.java
@@ -4,6 +4,8 @@ import com.example.tp02.models.Comentarios;
 import com.example.tp02.models.Filmes;
 import com.example.tp02.repository.ComentariosRepository;
 import com.example.tp02.repository.FilmesRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @RequestMapping("/comentarios")
 @RestController
+@Api(value = "API REST PARA LISTAGEM E  CRIACAO DE COMENTARIOS")
+@CrossOrigin(origins = "*")
 public class ComentariosControllers {
 
     @Autowired
@@ -21,6 +25,7 @@ public class ComentariosControllers {
     FilmesRepository filmesRepository;
 
     @GetMapping(value = "/filmes/{id_filme}", produces = "application/json")
+    @ApiOperation(value = "Todos o comentarios de um filme")
     public List<Comentarios> index(@PathVariable long id_filme){
 
         Filmes filme = filmesRepository.findById(id_filme);
@@ -30,6 +35,7 @@ public class ComentariosControllers {
 
     @PostMapping(value="/filmes/{id_filme}", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Cria um comentario para um filme")
     public ResponseEntity <Comentarios> create(@RequestBody Comentarios comentario, @PathVariable long id_filme){
         Filmes filme = filmesRepository.findById(id_filme);
         if(filme == null){
--- src/main/java/com/example/tp02/controllers/FilmesController.java
+++ src/main/java/com/example/tp02/controllers/FilmesController.java
@@ -4,27 +4,34 @@ import java.util.List;
 
 import com.example.tp02.models.Filmes;
 import com.example.tp02.repository.FilmesRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/filmes")
+@Api(value = "API REST PARA LISTAGEM DE FILMES")
+@CrossOrigin(origins = "*")
 public class FilmesController {
     @Autowired
     FilmesRepository filmesRepository;
 
     @GetMapping
+    @ApiOperation(value = "Retorna a lista de filmes")
     public ResponseEntity<List<Filmes>> index(){
 
         return ResponseEntity.ok().body(filmesRepository.findAll());
     }
     @GetMapping("/{id_filme}")
+    @ApiOperation(value = "Retorna um filme")
     public ResponseEntity<Filmes> show(@PathVariable(value = "id_filme") long id_filme){
         return  ResponseEntity.ok().body(filmesRepository.findById(id_filme));
     }
 
     @PutMapping("/gostei/{id_filme}")
+    @ApiOperation(value = "Incrementa um like para um filme")
     public ResponseEntity <Filmes> gostei(@PathVariable(value = "id_filme") long id_filme){
         Filmes filme = filmesRepository.findById(id_filme);
         if(filme == null){
@@ -35,6 +42,7 @@ public class FilmesController {
         return ResponseEntity.ok(filmesRepository.save(filme));
     }
     @PutMapping("/nao_gostei/{id_filme}")
+    @ApiOperation(value = "Incrementa um dislike para um filme")
     public ResponseEntity <Filmes> naoGostei(@PathVariable(value = "id_filme") long id_filme){
         Filmes filme = filmesRepository.findById(id_filme);
         if(filme == null){
