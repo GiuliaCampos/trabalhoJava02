7e17fd2e8316a9e6aae91911fa62c07b59342ef7
Weslley de Campos <weslley082@gmail.com>
2020-08-18 15:26:28 -0300

    Testando

--- src/main/java/com/example/tp02/controllers/FilmesController.java
+++ src/main/java/com/example/tp02/controllers/FilmesController.java
@@ -21,7 +21,6 @@ public class FilmesController {
     @GetMapping
     @ApiOperation(value = "Retorna a lista de filmes")
     public ResponseEntity<List<Filmes>> index(){
-
         return ResponseEntity.ok().body(filmesRepository.findAll());
     }
     @GetMapping("/{id_filme}")
