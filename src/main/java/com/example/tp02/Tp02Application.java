254b18029b6f36b31843c484ee5cd648810f489f
Weslley de Campos <weslley082@gmail.com>
2020-08-18 23:01:19 -0300

    [FIX] Aviso na pagina inicial

--- src/main/java/com/example/tp02/Tp02Application.java
+++ src/main/java/com/example/tp02/Tp02Application.java
@@ -4,11 +4,17 @@ package com.example.tp02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Tp02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp02Application.class, args);
+	}
+	@GetMapping("/")
+	public String hello() {
+		String var =  "https://blogfilme.herokuapp.com/swagger-ui.html para ver a documentação";
+		return var;
 	}
 }
