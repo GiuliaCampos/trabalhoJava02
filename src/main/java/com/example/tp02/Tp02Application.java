97ffee2edbf3d05c59bb29ef515301b68a80a8db
Weslley de Campos <weslley082@gmail.com>
2020-08-18 23:03:48 -0300

    [FIX] Aviso na pagina inicial

--- src/main/java/com/example/tp02/Tp02Application.java
+++ src/main/java/com/example/tp02/Tp02Application.java
@@ -12,9 +12,5 @@ public class Tp02Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Tp02Application.class, args);
 	}
-	@GetMapping("/")
-	public String hello() {
-		String var =  "https://blogfilme.herokuapp.com/swagger-ui.html para ver a documentação";
-		return var;
-	}
+
 }
