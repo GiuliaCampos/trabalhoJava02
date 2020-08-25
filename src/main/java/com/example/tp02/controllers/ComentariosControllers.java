adb354cfdcdaec0b5b351f89e27308dd2469f33e
Weslley de Campos <weslley082@gmail.com>
2020-08-25 19:22:10 -0300

    Alterando aqui

--- src/main/java/com/example/tp02/controllers/ComentariosControllers.java
+++ src/main/java/com/example/tp02/controllers/ComentariosControllers.java
@@ -2,8 +2,10 @@ package com.example.tp02.controllers;
 
 import com.example.tp02.models.Comentarios;
 import com.example.tp02.models.Filmes;
+import com.example.tp02.models.Usuario;
 import com.example.tp02.repository.ComentariosRepository;
 import com.example.tp02.repository.FilmesRepository;
+import com.example.tp02.repository.UsuarioRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,8 @@ public class ComentariosControllers {
     ComentariosRepository comentariosRepository;
     @Autowired
     FilmesRepository filmesRepository;
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
     @GetMapping(value = "/filmes/{id_filme}", produces = "application/json")
     @ApiOperation(value = "Todos o comentarios de um filme")
@@ -32,15 +36,23 @@ public class ComentariosControllers {
         return comentariosRepository.findComentariosByFilme(filme);
     }
 
-    @PostMapping(value="/filmes/{id_filme}", consumes = "application/json", produces = "application/json")
+    @PostMapping(value="/filmes/{id_filme}/usuario/{id_usuario}", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Cria um comentario para um filme")
-    public ResponseEntity <Comentarios> create(@RequestBody Comentarios comentario, @PathVariable long id_filme){
+    public ResponseEntity <Comentarios> create(@RequestBody Comentarios comentario,@PathVariable(value = "id_usuario")  long id_usuario ,@PathVariable(value = "id_filme")  long id_filme){
         Filmes filme = filmesRepository.findById(id_filme);
+        System.out.println(comentario.getComentario());
         if(filme == null){
             return ResponseEntity.notFound().build();
         }
+        Usuario usuario = usuarioRepository.findById(id_usuario);
+
+        if(usuario == null){
+            return ResponseEntity.notFound().build();
+        }
         comentario.setFilme(filme);
+        comentario.setUsuario(usuario);
+
 
         return ResponseEntity.ok(comentariosRepository.save(comentario));
     }
